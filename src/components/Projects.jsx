7cf97a6e67ec5b0cc4ce9e28287517f6a7dd9b4a
Samaelish <semnd007@gmail.com>
2024-09-04 21:44:20 +0300

    adapted the text into Russian

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,8 +6,8 @@ function Projects() {
   return (
     <main className='container mx-auto max-width pt-10 mb-20'>
       <section>
-        <h1 className='text-center text-2xl text-dark-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold'>Projects</h1>
-        <p className='text-center text-content py-2'>Things I&apos;ve built so far</p>
+        <h1 className='text-center text-2xl text-dark-heading md:text-4xl xl:text-5xl xl:leading-tight font-bold'>Проекты</h1>
+        <p className='font-semibold text-center text-content py-2'>Те проекты, которые я создал недавно</p>
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-10'>
           {React.Children.toArray(
             projectDetails.map(({ title, image, description, techstack, previewLink, githubLink }) => (
