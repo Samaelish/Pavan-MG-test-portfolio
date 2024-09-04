f8472cbf32465a29c935e197fc28c5c84d2d6679
Samaelish <semnd007@gmail.com>
2024-09-04 21:44:34 +0300

    adapted the text into Russian

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,10 @@ function Home() {
   const { name, tagline, img } = personalDetails
 
   return (
-    <main className='container mx-auto max-width section md:flex justify-between items-center'>
+    <main className='container mb-10 mx-auto max-width section md:flex justify-between items-center'>
       <div>
         <h1 className='text-2xl text-dark-heading  md:text-4xl xl:text-5xl xl:leading-tight font-bold'>
-          Hi,👋<br></br>My Name is<br></br>
+          Привет,👋<br></br>Меня зовут<br></br>
         </h1>
         <h1 className='text-2xl bg-clip-text bg-gradient text-transparent md:text-4xl xl:text-5xl xl:leading-tight font-bold'>
           {name}
@@ -15,7 +15,7 @@ function Home() {
         <h2 className='text-2xl text-dark-heading  md:text-4xl xl:text-5xl xl:leading-tight font-bold'>{tagline}</h2>
       </div>
       <div className='mt-5 md:mt-0'>
-        <img className='w-1/2 md:ml-auto' src={img} alt='Pavan MG' />
+        <img className='w-1/2 rounded-full md:ml-auto' src={img} alt='Pavan MG' />
       </div>
     </main>
   )
