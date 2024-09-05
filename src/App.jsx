a39221f430598c4d4f5291fb628150788478288a
Samaelish <semnd007@gmail.com>
2024-09-05 20:59:22 +0300

    removed old footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import Header from './components/Header'
-import Footer from './components/Footer'
 import Home from './components/Home'
 import Contact from './components/Contact'
 import Projects from './components/Projects'
@@ -13,7 +12,6 @@ function App() {
       <Technologies />
       <Projects />
       <Contact />
-      <Footer />
     </>
   )
 }
