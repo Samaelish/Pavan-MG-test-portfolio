b9311cf203418e99c9ef7f777daf22e44de2ccfa
Samaelish <semnd007@gmail.com>
2024-09-05 21:00:24 +0300

    changed gradient text in home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,12 +7,11 @@ function Home() {
     <main className='container mb-10 mx-auto max-width section md:flex justify-between items-center'>
       <div>
         <h1 className='text-2xl text-dark-heading  md:text-4xl xl:text-5xl xl:leading-tight font-bold'>
-          Привет,👋<br></br>Меня зовут<br></br>
+          Привет,👋
+          <br />
+          Меня зовут
+          <br /> {name} {tagline}
         </h1>
-        <h1 className='text-2xl bg-clip-text bg-gradient text-transparent md:text-4xl xl:text-5xl xl:leading-tight font-bold'>
-          {name}
-        </h1>
-        <h2 className='text-2xl text-dark-heading  md:text-4xl xl:text-5xl xl:leading-tight font-bold'>{tagline}</h2>
       </div>
       <div className='mt-5 md:mt-0'>
         <img className='w-1/2 rounded-full md:ml-auto' src={img} alt='Pavan MG' />
