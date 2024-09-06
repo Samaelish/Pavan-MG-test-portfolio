f142392a90be821eb3d437101fcd6e85973a6207
Samaelish <semnd007@gmail.com>
2024-09-06 21:49:37 +0300

    home refined

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,14 @@ function Home() {
   const { name, tagline, img } = personalDetails
 
   return (
-    <main className='container mb-10 mx-auto max-width section md:flex justify-between items-center'>
+    <main className='container mb-10 mx-auto max-width section md:flex justify-between items-center' id='home'>
       <div>
         <h1 className='text-2xl text-dark-heading  md:text-4xl xl:text-5xl xl:leading-tight font-bold'>
           Привет,👋
           <br />
           Меня зовут
-          <br /> {name} {tagline}
+          <br /> <span className='bg-clip-text bg-gradient text-transparent'>{name}</span>
+          <br /> {tagline}
         </h1>
       </div>
       <div className='mt-5 md:mt-0'>
